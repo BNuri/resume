4736b2942ce71d14ccf7ad2aeda61407a8151eba
BNURI <luckmon05@naver.com>
2020-05-16 05:13:16 +0900

    change includes to indexOf

--- es6/darkToggle.js
+++ es6/darkToggle.js
@@ -2,9 +2,10 @@ const darkToggle = (event) => {
   const body = document.body;
   const toggleBtn = event.target;
   body.classList.toggle("dark-mode");
-  toggleBtn.className = body.className.includes("dark-mode")
-    ? "fas fa-sun fa-lg"
-    : "fas fa-moon fa-lg";
+  toggleBtn.className =
+    body.className.indexOf("dark-mode") > 0
+      ? "fas fa-sun fa-lg"
+      : "fas fa-moon fa-lg";
 };
 
 const darkBtn = document.getElementById("darkToggle");
--- js/darkToggle.js
+++ js/darkToggle.js
@@ -4,7 +4,7 @@ var darkToggle = function darkToggle(event) {
   var body = document.body;
   var toggleBtn = event.target;
   body.classList.toggle("dark-mode");
-  toggleBtn.className = body.className.includes("dark-mode") ? "fas fa-sun fa-lg" : "fas fa-moon fa-lg";
+  toggleBtn.className = body.className.indexOf("dark-mode") > 0 ? "fas fa-sun fa-lg" : "fas fa-moon fa-lg";
 };
 
 var darkBtn = document.getElementById("darkToggle");
